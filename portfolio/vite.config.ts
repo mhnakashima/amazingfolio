eb67a827d161b16048513fa38fef329c08aebe9b
michael nakashima <michael.nakashima-ext2@ab-inbev.com>
2025-08-03 19:34:59 -0300

    [portfolio] adding portfolio configuration

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import federation from '@originjs/vite-plugin-federation'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
+    tailwindcss(),
     federation({
       name: 'portfolio',
       filename: 'remoteEntry.js',
